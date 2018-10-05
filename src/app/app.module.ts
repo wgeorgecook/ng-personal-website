28123c2377ece6c2763e710b24061256addbbe24
William George Cook <wgeorgecook@gmail.com>
2018-10-04 21:35:09 -0700

    Importing MatDialogModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {MatDialogModule} from '@angular/material';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
