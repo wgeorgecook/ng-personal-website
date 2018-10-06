ed336ca0c08aca0fced445f2bbb83d037283c1c7
William George Cook <wgeorgecook@gmail.com>
2018-10-06 11:21:12 -0700

    Importing contact data from json file

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,13 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import * as data from '../../assets/data.json'
 
 @Component({
   selector: 'app-contact',
-  templateUrl: './contact.component.html',
+  templateUrl: './contact.component.template.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  contacts: Array<string>;
 
-  constructor() { }
+  constructor() {
+    this.contacts = (<any>data).contact
+  }
 
   ngOnInit() {
   }
